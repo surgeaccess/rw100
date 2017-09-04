3e405064d44c570aac98a58f3d6db36c77b24baf
lukerealwear <luke@realwear.com>
2017-09-04 11:53:12 +0100

    Merge

--- app/src/main/assets/sample/website/js/wearml_engine.js
+++ app/src/main/assets/sample/website/js/wearml_engine.js
@@ -137,11 +137,7 @@ function generateXML(){
       var style = getStyle(styleId)
 
       if(style != undefined){
-<<<<<<< HEAD
          xml += wearMLParser(style, wearMLElements[i]);
-=======
-			xml += wearMLParser(style, wearMLElements[i]);
->>>>>>> 6a1e9f56339c7ffe25de135b2047ac72d070aaad
       }
 
       if(command != undefined){
